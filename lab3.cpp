9dbe0af8c1b4daf503e55dc7f3988ca2e4d2d9ff
KontradZebrowsk <konzebr@gmail.com>
2020-11-18 22:00:44 +0000

    I tried to code it right

--- lab3.cpp
+++ lab3.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 
+class Figura
+{
+  protected:
+    double pole = 0.;
+  public:
+    /*void set_pole(double p)
+    {
+      pole = p;
+    }*/
+    double get_pole()
+    {
+      return pole;
+    }
+  
+};
+
+class Kolo : Figura 
+{
+  Kolo(double r)
+  {
+    //double p =r*r;
+    //set_pole(p);
+    pole = r*r;
+  }
+};
+class Kwadrat : Figura 
+{
+  Kwadrat(double a, double b)
+  {
+    //double p = a*b;
+    //set_pole(p);
+    pole = a*b;
+  }
+};
+
 int main()
 {
     puts("Hello, World!");
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -5,5 +5,19 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
+    unsigned int przetransportowane = 0;
+    unsigned int liczba_zaglowcow = 0;
+    while(towar > przetransportowane)
+    {
+      Statek* s1 = stocznia();
+      if (dynamic_cast<Zaglowiec*>(s1) != nullptr)
+      {
+        liczba_zaglowcow = liczba_zaglowcow + 1;
+      }
+      unsigned int dostarczone = s1.transportuj();
+      przetransportowane = przetransportowane + dostarczone;
+      virtual delete s1;
+    }
+    
     return 0;
 }
