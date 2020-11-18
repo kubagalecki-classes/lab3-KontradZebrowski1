d6c1e7abf71f5e536398a440473ab218f6986abb
KontradZebrowsk <konzebr@gmail.com>
2020-11-18 22:11:33 +0000

    zmiany nazw zaglowce

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -7,13 +7,13 @@ unsigned int transportujFlota(unsigned int towar)
     Stocznia stocznia{};
     // Twoja implementacja tutaj
     unsigned int przetransportowane = 0;
-    unsigned int liczba_zaglowcow = 0;
+    unsigned int zaglowce = 0;
     while(towar > przetransportowane)
     {
       Statek* s1 = stocznia();
       if (dynamic_cast<Zaglowiec*>(s1) != nullptr)
       {
-        liczba_zaglowcow = liczba_zaglowcow + 1;
+        zaglowce = zaglowce + 1;
       }
       unsigned int dostarczone = s1->transportuj();
       przetransportowane = przetransportowane + dostarczone;
